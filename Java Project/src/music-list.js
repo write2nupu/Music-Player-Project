5ca33c0d0b30ed9773b7e1a87fa26c09d4e7acf7
Nupur Sharma <69757739+write2nupu@users.noreply.github.com>
2024-04-08 20:47:23 +0530

    Add files via upload

---
+++ Java Project/src/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+      name: "Hai Junoon",
+      artist: "KK, Monali Thakur, Pritam",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Ilahi",
+      artist: "Arijit Singh",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Sooraj Dooba Hai",
+      artist: "Arijit Singh",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Cruel Summer",
+      artist: "Taylor Swift",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Let Me Love You",
+      artist: "DJ Snake, ft. Justin Bieber",
+      img: "music-3",
+      src: "music-3"
+    }
+  ];
